4aa7bea8d6a9857dda02783528658e2292590677
milindhegde <milind.hegde@gmail.com>
2025-10-02 15:25:46 +0800

    Add files via upload

---
+++ info.js
@@ -0,0 +1,20 @@
+//clicky stuff
+try{ clicky.init(100890482); }catch(e){}
+
+//statcounter stuff
+var sc_project=10678443; 
+var sc_invisible=1; 
+var sc_security="bfde9015"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T9QP3WD2');</script>
+<!-- End Google Tag Manager -->
